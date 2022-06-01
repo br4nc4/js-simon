0b48f9bf515f360e4ba1bea0851e7dd54def75ec
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-01 08:43:07 +0200

    details + restart confronto (includes)

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 //variabili globali
 const numbersContainer = document.getElementById("numbersContainer");
-const enteredNumbers=[];
-const readNumbersMemory = [];
+const userNumbers=[];
+const memoryNumbers = [];
 const numIndovinati =[];
 
-const countDown = document.getElementById("time");
+const numbers = document.createElement("div");
+
+const countDown = document.getElementById("timer");
 let secondiRimanenti = 30;
 
 //funzione per generare 5 numeri casuali
 function randomNumbers(){
     for(let i=0; i<5; i++){
         const random = Math.floor(Math.random() * 200)+1;
-        readNumbersMemory.push(random);
+        memoryNumbers.push(random);
     }
-    return readNumbersMemory;
+    //stampare a video i numeri dell'array memoryNumbers
+    numbers.innerText = memoryNumbers;
+    numbers.classList.add("numbers");
+    numbersContainer.append(numbers);
 }
 
-//stampare a video i numeri dell'array readNumbersMemory
-const numbers = document.createElement("div");
-numbers.innerText = randomNumbers();
-numbers.classList.add("numbers");
-numbersContainer.append(numbers);
+randomNumbers();
 
 //funzione che visualizza a schermo il countDown di 30s
 //raggiunto lo scadere del tempo
 //secondi = 0 i numeri visualizzati scompaiono
-const hideNumbers = setInterval(function(){
+const hidenNumbers = setInterval(function(){
     secondiRimanenti--;
     countDown.innerText = secondiRimanenti;
     if(secondiRimanenti == 0){
         numbers.classList.add("nullNumber")
-        clearInterval(hideNumbers);
+        clearInterval(hidenNumbers);
     }
 },1000)
 
@@ -39,12 +40,15 @@ const hideNumbers = setInterval(function(){
 //questa funzione si attiva dopo 30 secondi
 setTimeout(requestUserNumbers, 31000);
 
-function requestUserNumbers(readNumbersMemory){
+function requestUserNumbers(memoryNumbers){
     //da prompt chiedere per 5 volte dei numeri
     for (let i =0; i<5; i++){
     const inputNumbers = prompt("inserisci gli stessi numeri visualizzati a schermo");
-    enteredNumbers.push(parseInt(inputNumbers));
-    //console.log(enteredNumbers);
+    userNumbers.push(parseInt(inputNumbers));
+    //console.log(userNumbers);
+    if(memoryNumbers.includes(userNumbers[i])){
+        numIndovinati.push(userNumbers[i])
+    }
     }
 } 
 
